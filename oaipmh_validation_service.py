749d7ed4691d77c2fac61a9d2f4e446c9a1e4bd8
Simeon Warner <simeon.warner@cornell.edu>
2015-10-14 23:22:15 -0400

    A start with recaptcha

---
+++ oaipmh_validation_service.py
@@ -0,0 +1,51 @@
+"""
+OAI-PMH Validation oaipmh_validation_service
+
+Work to replace https://www.openarchives.org/Register/ValidateSite
+
+reCaptcha support based on flask-wtf example code from
+https://github.com/lepture/flask-wtf/tree/master/examples/recaptcha
+"""
+from flask import Flask, render_template, flash, session, redirect, url_for
+from wtforms import TextAreaField
+from wtforms.validators import DataRequired
+from flask.ext.wtf import Form
+from flask.ext.wtf.recaptcha import RecaptchaField
+import os.path
+
+app = Flask(__name__)
+cfg = 'oaipmh_validation_service.cfg'
+app.config.from_pyfile(cfg+'_default') #assume default always present
+app.config.from_pyfile(cfg, silent=True) #override for real config
+
+class ValidateForm(Form):
+
+    base_url = TextAreaField("baseURL", validators=[DataRequired()])
+    recaptcha = RecaptchaField()
+
+@app.route("/")
+def index():
+    return redirect(url_for("validate"))
+
+@app.route("/pmh/validate")
+def validate(form=None):
+    if form is None:
+        form = ValidateForm()
+    base_url = session.get("base_url", [])
+    return render_template("validate.html",
+                           base_url=base_url,
+                           form=form)
+
+@app.route("/pmh/validate/run", methods=("POST",))
+def run_validate():
+    form = ValidateForm()
+    if form.validate_on_submit():
+        session['base_url'] = form.base_url.data
+        return render_template("validate_run.html",
+                               base_url=form.base_url.data)
+    else:
+        # go back and try again...
+        return redirect(url_for("validate"))
+
+if __name__ == "__main__":
+    app.run()
